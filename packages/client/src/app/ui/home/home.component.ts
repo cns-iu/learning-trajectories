ead9d4a2ea073bd9eab567f687c91a4d926e4f63
Gagandeep Singh <singhgag@indiana.edu>
2018-05-18 15:21:30 -0400

    Some code reorganization for the UI part for #5

--- packages/client/src/app/ui/home/home.component.ts
+++ packages/client/src/app/ui/home/home.component.ts
@@ -2,7 +2,6 @@ import {
   Component, 
   OnInit
 } from '@angular/core';
-import { MatButtonToggleChange } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-home',
@@ -11,33 +10,9 @@ import { MatButtonToggleChange } from '@angular/material/button-toggle';
 })
 export class HomeComponent implements OnInit {
   openState = true;
-  playPauseIcon = 'play_arrow';
-  
-  persons = [
-    {value: 'person-0', viewValue: 'Person 0'},
-    {value: 'person-1', viewValue: 'Person 1'},
-    {value: 'person-2', viewValue: 'Person 2'}
-  ];
 
   constructor() { }
 
   ngOnInit() {
   }
-
-  onPlayToggle() {
-    switch(this.playPauseIcon) {
-      case 'play_arrow' :
-        this.playPauseIcon = 'pause';
-        break;
-      case 'pause':
-        this.playPauseIcon = 'play_arrow';
-        break;
-      default: this.playPauseIcon = 'play_arrow';
-    }
-  }
-
-  onStop() {
-    this.playPauseIcon = 'play_arrow';
-  }
-
 }
